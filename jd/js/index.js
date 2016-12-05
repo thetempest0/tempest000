332d32bb4fae243f5dc6b85039a16adfbac09801
thetempest0 <292810794@qq.com>
2016-12-05 23:36:20 +0800

    第五天

--- jd/js/index.js
+++ jd/js/index.js
@@ -200,7 +200,7 @@
 			$('.service_txt').eq($(this).index()).css('border-top-color', 'red').parents('.tant').siblings().find('.service_txt').css('border-top-color', 'white');
 			$('.tanchu').stop().animate({
 				top: 30
-			},200);
+			}, 200);
 		});
 		$('.xx').on('click', function() {
 			$('.tanchu').stop().animate({
@@ -225,221 +225,391 @@
 	//	---------------------这是秒杀阶段-----------------------
 	(function() {
 		$.ajax({
-			url:"json/index.json",
-		})
-		.done(function  (data) {
-			$('.sip').each(function  (i) {
-				$(this).attr('src',data.sk[i].img);
-			});
-			$('.sipp').each(function  (i) {
-				$(this).html(data.sk[i].title);
-			});
-			$('.sk_newprice span').each(function  (i) {
-				$(this).html(data.sk[i].price);
-			});
-			$('.sk_price del').each(function  (i) {
-				$(this).html(data.sk[i].orign);
-			});
-			$('.sip').hover(function  () {
-				$(this).stop().animate({top:-3},200)
-			},function  () {
-				$(this).stop().animate({top:0},200)
+				url: "json/index.json",
 			})
-		})
-	})();
-	
-//	---------------------这是秒杀阶段-----------------------
-		(function () {
-			$.ajax({
-				url:"json/index.json",
-			})
-			.done(function  (data) {
-				$('.fcg-img').each(function  (i) {
-					$(this).attr('src',data.fcg[i].img);
+			.done(function(data) {
+				$('.sip').each(function(i) {
+					$(this).attr('src', data.sk[i].img);
 				});
-				$('.find_item_name').each(function  (i) {
+				$('.sipp').each(function(i) {
+					$(this).html(data.sk[i].title);
+				});
+				$('.sk_newprice span').each(function(i) {
+					$(this).html(data.sk[i].price);
+				});
+				$('.sk_price del').each(function(i) {
+					$(this).html(data.sk[i].orign);
+				});
+				$('.sip').hover(function() {
+					$(this).stop().animate({
+						top: -3
+					}, 200)
+				}, function() {
+					$(this).stop().animate({
+						top: 0
+					}, 200)
+				})
+			})
+	})();
+
+	//	---------------------这是秒杀阶段-----------------------
+	(function() {
+		$.ajax({
+				url: "json/index.json",
+			})
+			.done(function(data) {
+				$('.fcg-img').each(function(i) {
+					$(this).attr('src', data.fcg[i].img);
+				});
+				$('.find_item_name').each(function(i) {
 					$(this).html(data.fcg[i].title);
 				});
-				$('.fcg-img').hover(function  () {
-					$(this).stop().animate({right:15},200)
-				},function  () {
-					$(this).stop().animate({right:10},200)
+				$('.fcg-img').hover(function() {
+					$(this).stop().animate({
+						right: 15
+					}, 200)
+				}, function() {
+					$(this).stop().animate({
+						right: 10
+					}, 200)
 				});
 			})
-		})();
-		//	---------------------这是品牌阶段-----------------------
-		(function () {
-			$.ajax({
-				url:"json/index.json",
-				async:true
+	})();
+	//	---------------------这是品牌阶段-----------------------
+	(function() {
+		$.ajax({
+				url: "json/index.json",
+				async: true
 			})
-			.done(function  (data) {
-				$('.brand_rec_img').each(function  (i) {
-				$(this).attr('src',data.pinp[i].img);
-			});
-			$('.brand_rec_img').hover(function  () {
-					$(this).animate({right:5},200);
-				
-			},function  () {
-					$(this).animate({right:0},200);
-			});
-				$('.brand_rec_name').each(function  (i) {
-				$(this).html(data.pinp[i].title);
-			});
-				$('.brand_rec_desc').each(function  (i) {
-				$(this).html(data.pinp[i].desc);
-			});
-			})
-		})();
-		//	---------------------这是排行榜阶段-----------------------
-		(function () {
-			$.ajax({
-				type:"get",
-				url:"json/index.json",
-				async:true
-			})
-			.done(function  (data) {
-				$('.pvi_img').each(function  (i) {
-					$(this).attr('src',data.pvi[i].img);
+			.done(function(data) {
+				$('.brand_rec_img').each(function(i) {
+					$(this).attr('src', data.pinp[i].img);
 				});
-				$('.pvi_name').each(function  (i) {
+				$('.brand_rec_img').hover(function() {
+					$(this).animate({
+						right: 5
+					}, 200);
+
+				}, function() {
+					$(this).animate({
+						right: 0
+					}, 200);
+				});
+				$('.brand_rec_name').each(function(i) {
+					$(this).html(data.pinp[i].title);
+				});
+				$('.brand_rec_desc').each(function(i) {
+					$(this).html(data.pinp[i].desc);
+				});
+			})
+	})();
+	//	---------------------这是排行榜阶段-----------------------
+	(function() {
+		$.ajax({
+				type: "get",
+				url: "json/index.json",
+				async: true
+			})
+			.done(function(data) {
+				$('.pvi_img').each(function(i) {
+					$(this).attr('src', data.pvi[i].img);
+				});
+				$('.pvi_name').each(function(i) {
 					$(this).html(data.pvi[i].title);
 				})
 			})
-			
-		})();
-		//	---------------------这是排行榜效果-----------------------
-		(function () {
-			$('.paih_a').on('mouseover',function  () {
-				$('.paih_nva_item').eq($(this).index()).show().siblings('div').hide();
-			});
-			
-		})();
-		//	---------------------这是倒计时---------------------------
-		(function () {
-			 function dou(value){
-        	if(value<10){
-        		return '0'+value;
-        	}else{
-        		return value;
-        	}
-        }
-			 function djs(){
-			var future = new Date(2016, 11, 30, 00, 00, 00);
-	        var t = (future - new Date()) / 1000;
-	        var h = Math.floor(t % 86400 / 3600);
-	        var m = Math.floor(t % 86400 % 3600 / 60);
-	        var s = parseInt(t % 60);
-	        $('.hour').html(dou(h));
-	        $('.minute').html(dou(m));
-	        $('.second').html(dou(s));
+
+	})();
+	//	---------------------这是排行榜效果-----------------------
+	(function() {
+		$('.paih_a').on('mouseover', function() {
+			$('.paih_nva_item').eq($(this).index()).show().siblings('div').hide();
+		});
+
+	})();
+	//	---------------------这是倒计时---------------------------
+	(function() {
+		function dou(value) {
+			if(value < 10) {
+				return '0' + value;
+			} else {
+				return value;
+			}
 		}
-			 djs();
-			 setInterval(djs,1000);
-		})();
-		//	---------------------这是倒计时---------------------------
-		(function () {
-			$.ajax({
-				type:"get",
-				url:"json/index.json",
-				async:true
+
+		function djs() {
+			var future = new Date(2016, 11, 30, 00, 00, 00);
+			var t = (future - new Date()) / 1000;
+			var h = Math.floor(t % 86400 / 3600);
+			var m = Math.floor(t % 86400 % 3600 / 60);
+			var s = parseInt(t % 60);
+			$('.hour').html(dou(h));
+			$('.minute').html(dou(m));
+			$('.second').html(dou(s));
+		}
+		djs();
+		setInterval(djs, 1000);
+	})();
+	//	---------------------这是倒计时---------------------------
+	(function() {
+		$.ajax({
+				type: "get",
+				url: "json/index.json",
+				async: true
 			})
-			.done(function  (data) {
-				$('.coupon_img').each(function  (i) {
-					$(this).attr('src',data.lq[i].img);
+			.done(function(data) {
+				$('.coupon_img').each(function(i) {
+					$(this).attr('src', data.lq[i].img);
 				});
-				$('.coupon_jg').each(function  (i) {
+				$('.coupon_jg').each(function(i) {
 					$(this).html(data.lq[i].price);
 				});
-				$('.coupon_desc').each(function  (i) {
+				$('.coupon_desc').each(function(i) {
 					$(this).html(data.lq[i].desc);
 				});
-				$('.coupon_limit').each(function  (i) {
+				$('.coupon_limit').each(function(i) {
 					$(this).html(data.lq[i].title);
 				});
-				$('.coupon_more').each(function  (i) {
+				$('.coupon_more').each(function(i) {
 					$(this).html(data.lq[i].more);
 				});
-				$('.coupon_img').hover(function  () {
-					$(this).stop().animate({right:0},200);
-				},function  () {
-					$(this).stop().animate({right:15},200);
+				$('.coupon_img').hover(function() {
+					$(this).stop().animate({
+						right: 0
+					}, 200);
+				}, function() {
+					$(this).stop().animate({
+						right: 15
+					}, 200);
 				});
 			})
-		})();
-		//	---------------------这是减价---------------------------
-		(function () {
-			$.ajax({
-				type:"get",
-				url:"json/index.json",
-				async:true
+	})();
+	//	---------------------这是减价---------------------------
+	(function() {
+		$.ajax({
+				type: "get",
+				url: "json/index.json",
+				async: true
 			})
-			.done(function  (data) {
-				$('.rec_img').each(function  (i) {
-					$(this).attr('src',data.rec[i])
+			.done(function(data) {
+				$('.rec_img').each(function(i) {
+					$(this).attr('src', data.rec[i])
 				});
-				$('.rec_img').hover(function  () {
-					$(this).stop().animate({opacity:0.8});
-				},function  () {
-					$(this).stop().animate({opacity:1});
+				$('.rec_img').hover(function() {
+					$(this).stop().animate({
+						opacity: 0.8
+					});
+				}, function() {
+					$(this).stop().animate({
+						opacity: 1
+					});
 				});
 			})
-		})();
-		//	---------------------这是享品质阶段---------------------------
-		(function () {
-			$.ajax({
-				type:"get",
-				url:"json/index.json",
-				async:true
+	})();
+	//	---------------------这是享品质阶段---------------------------
+	(function() {
+		$.ajax({
+				type: "get",
+				url: "json/index.json",
+				async: true
 			})
-			.done(function  (data) {
-				$('.entry_img').each(function  (i) {
-					$(this).attr('src',data.xpz[i]);
+			.done(function(data) {
+				$('.entry_img').each(function(i) {
+					$(this).attr('src', data.xpz[i]);
 				});
-				$('.entry_img').hover(function  () {
-					$(this).stop().animate({right:0},200)
-				},function  () {
-					$(this).stop().animate({right:-10},200)
+				$('.entry_img').hover(function() {
+					$(this).stop().animate({
+						right: 0
+					}, 200)
+				}, function() {
+					$(this).stop().animate({
+						right: -10
+					}, 200)
 				});
 			});
-		})();
-		//	---------------------这是爱生活阶段---------------------------
-		(function () {
-			$.ajax({
-				type:"get",
-				url:"json/index.json",
-				async:true
+	})();
+	//	---------------------这是爱生活阶段---------------------------
+	(function() {
+		$.ajax({
+				type: "get",
+				url: "json/index.json",
+				async: true
 			})
-			.done(function  (data) {
-				$('.pt_cover_img').each(function  (i) {
-					$(this).attr('src',data.abig[i]);
+			.done(function(data) {
+				$('.pt_cover_img').each(function(i) {
+					$(this).attr('src', data.abig[i]);
 				});
-				$('.pt_cover_img').hover(function  () {
-					$(this).stop().animate({left:-10},150)
-				},function  () {
-					$(this).stop().animate({left:0},150)
+				$('.pt_cover_img').hover(function() {
+					$(this).stop().animate({
+						left: -10
+					}, 150)
+				}, function() {
+					$(this).stop().animate({
+						left: 0
+					}, 150)
 				});
-				$('.pt_bi_img').each(function  (i) {
-					$(this).attr('src',data.pic[i].img);
+				$('.pt_bi_img').each(function(i) {
+					$(this).attr('src', data.pic[i].img);
 				});
-				$('.pt_bi_img').hover(function  () {
-					$(this).stop().animate({right:8},150)
-				},function  () {
-					$(this).stop().animate({right:0},150)
+				$('.pt_bi_img').hover(function() {
+					$(this).stop().animate({
+						right: 8
+					}, 150)
+				}, function() {
+					$(this).stop().animate({
+						right: 0
+					}, 150)
 				});
-				$('.pt_bi_tit').each(function  (i) {
+				$('.pt_bi_tit').each(function(i) {
 					$(this).html(data.pic[i].title);
 				});
-				$('.pt_bi_promo').each(function  (i) {
+				$('.pt_bi_promo').each(function(i) {
 					$(this).html(data.pic[i].desc);
 				});
-				$('.pt_more_img').each(function  (i) {
-					$(this).attr('src',data.pt[i]);
+				$('.pt_more_img').each(function(i) {
+					$(this).attr('src', data.pt[i]);
 				});
-				$('.pt_more_img').hover(function  () {
-					$(this).stop().animate({right:10},150)
-				},function  () {
-					$(this).stop().animate({right:0},150)
+				$('.pt_more_img').hover(function() {
+					$(this).stop().animate({
+						right: 10
+					}, 150)
+				}, function() {
+					$(this).stop().animate({
+						right: 0
+					}, 150)
 				});
 			});
+	})();
+	//	---------------------这是多图轮播---------------------------
+	(function() {
+		var num = 1;
+		$('.lright1').on('click', function() {
+			num++;
+			tab('.duotu');
+		});
+		$('.lleft1').on('click', function() {
+			num--;
+			tab('.duotu');
+		});
+		$('.lright2').on('click', function() {
+			num++;
+			tab('.duotu2');
+		});
+		$('.lleft2').on('click', function() {
+			num--;
+			tab('.duotu2');
+		});
+
+		function tab(cla) {
+			$(cla).stop(true, true).animate({
+				left: -570 * num
+			}, 200, function() {
+				if($(cla).css('left') == -1710 + 'px') {
+					$(cla).css('left', '-570px');
+					num = 1;
+				}
+				if($(cla).css('left') == 0 + 'px') {
+					$(cla).css('left', '-1140px');
+					num = 2;
+				}
+
+			})
+		}
+	})();
+	//	---------------------这是家电手机阶段---------------------------
+	(function () {
+		$.ajax({
+			type:"get",
+			url:"html/costume.html",
+			async:true
+		})
+		.done(function  (data) {
+			$('#costume').append(data);
+		});
+	})();
+	//	---------------------这是3C爱吃阶段---------------------------
+	(function () {
+		$.ajax({
+			type:"get",
+			url:"html/eat.html",
+			async:true
+		})
+		.done(function  (data) {
+			$('#eat').append(data);
+		});
+	})();
+	//	---------------------这是母婴图书阶段---------------------------
+	(function () {
+		$.ajax({
+			type:"get",
+			url:"html/book.html",
+			async:true
+		})
+		.done(function  (data) {
+			$('#book').append(data);
+		});
+	})();
+	//	---------------------这是虚拟阶段---------------------------
+	(function () {
+		$.ajax({
+			type:"get",
+			url:"html/game.html",
+			async:true
+		})
+		.done(function  (data) {
+			$('#game').append(data);
+		});
+	})();
+	//	---------------------这是底部阶段---------------------------
+	(function () {
+		$.ajax({
+			type:"get",
+			url:"html/foot.html",
+			async:true
+		})
+		.done(function  (data) {
+			$('.Mod').append(data);
+		});
+	})();
+//	--------------------------固定栏-----------------------------------
+		(function () {
+			$(document).on('scroll',function  () {
+				if ($(document).scrollTop()>800) {
+					$('#search').stop().animate({top:0},200);
+				}else{
+					$('#search').stop().animate({top:-50},200);
+				}
+			});
+		})();
+//	--------------------------楼梯-----------------------------------
+		(function () {
+				$(window).scroll(function(){
+            var scrolltop=$(this).scrollTop();
+            if(scrolltop>=1600){
+                $('#loutinav').show();
+            }else{
+                $('#loutinav').hide();
+            }
+             $('.louti').each(function(){
+                var $loutitop=$('.louti').eq($(this).index()).offset().top+350;
+                if($loutitop>scrolltop){
+                    $('#loutinav li').removeClass('active');
+                    $('#loutinav li').eq($(this).index()).addClass('active');
+                    return false;
+                }
+            });
+        });
+            $('#loutinav li').not('.llast').click(function(){
+            $(this).addClass('active').siblings('li').removeClass('active');
+            var $loutitop=$('.louti').eq($(this).index()).offset().top;
+            $('html,body').stop(true).animate({
+                scrollTop:$loutitop-50
+            },600)
+        })
+            $('.llast').click(function(){
+            $('html,body').animate({
+                scrollTop:0
+            },600);
+        });
+			
 		})();
